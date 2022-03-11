0aec1a67c145b9b2027e0643b86004d4b1fb4341
Oluwaseyi-Java <adeseyitu@gmail.com>
2022-03-11 17:59:14 +0100

    Header done

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='header'>
+        <h1>WeatherApp</h1>
+        <div><p>Developed by:<br/><a href='google.com'>Oluwaseyi Adesunloro</a></p></div>
+    </div>
+  )
+}
+
+export default header
---
+++ src/components/search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import close from "./image/close.png"
+
+const search = () => {
+  return (
+    <div className='search'>
+        <input type="text" placeholder="Search"/>
+    </div>
+  )
+}
+
+export default search
