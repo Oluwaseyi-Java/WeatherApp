cbeb40356e35a03d5d8716df2f0a001612e4eaf8
Oluwaseyi-Java <adeseyitu@gmail.com>
2022-03-20 00:59:22 +0100

    Added button to developer page

--- src/components/developer.jsx
+++ src/components/developer.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './header'
+import { Link } from 'react-router-dom'
+
 
 const Developer = () => {
   return (
     <div className='developer'> 
         <Header/>
         <p id='message'> The section will be updated later.</p>
-        <button>Go Back</button>
+        <Link to="/">
+          <button >Go Back</button>
+        </Link>
     </div>
     )
 }
