fb6042c6608b05e89914ea2f38ff8cfd0cf27fd0
Oluwaseyi-Java <adeseyitu@gmail.com>
2022-03-19 23:31:17 +0100

    Done with the data fetching. About to finish up

--- src/App.js
+++ src/App.js
@@ -9,35 +9,56 @@ import LocationText from './components/location-text';
 
 function App() {
 
-  // let [responseData, setResponseData] = React.useState('')
-  // let [message, setMessage] = React.useState('')
-
-  
+  let [responseData, setResponseData] = React.useState('')
+  let [message, setMessage] = React.useState('')
+  let [country, setCountry]=React.useState("")
+  let [main, setMain]=React.useState("")
+  let [weather, setWeather]=React.useState("")
+  let [wind, setWind]=React.useState("")
 
   const onAdd = (value) => {
 
-
-    // let ID = "75d4d68ab258bd16c2defa27bb9c5a7c"
-    // let url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${ID}`
-    //   setMessage('Loading...')
-    // axios(url)
-    //   .then((response)=>{
-    //     setResponseData(response.data)
-    //     setMessage('')
-    //     console.log(response)
-    //   })
-    //   .catch((error) => {
-    //     setMessage('Error')
-    //     console.log(error)
-    //   })
+    let ID = "75d4d68ab258bd16c2defa27bb9c5a7c"
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${ID}`
+      setMessage('Loading...')
+    axios(url)
+      .then((response)=>{
+        setResponseData(response.data)
+        setCountry(response.data.sys)
+        setMain(response.data.main)
+        setWeather(response.data.weather[0])
+        setWind(response.data.wind)
+        setMessage('')
+        console.log(response.data)
+      })
+      .catch((error) => {
+        setMessage('Not found')
+        console.log(error)
+      })
   }
+
+  // fetch('https://api.ipregistry.co/?key=tryout')
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (payload) {
+  //     console.log(payload.location.country.name + ', ' + payload.location.city);
+  //   });
 
   return (
     <div className="App">
       <Header/>
       <Search onPressed={onAdd} />
-      <LocationText/>
-      <WeatherCard/>
+      <LocationText />
+      <WeatherCard 
+        wind={wind}
+        weather={weather}
+        main={main}
+        country={country}
+        messageSent={message} 
+        data={responseData}
+        
+      />
     </div>
   );
 }
--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
 
-const weatherCard = () => {
+const WeatherCard = (props) => {
 
-
+  const temp=()=>{
+    return Math.floor(`${(props.main.temp) -273.15}`)
+  }
+  const temp_min=()=>{
+    return Math.floor(`${(props.main.temp_min) -273.15}`)
+  }
+  const temp_max=()=>{
+    return Math.floor(`${(props.main.temp_max) -273.15}`)
+  }
+ 
   
+      // <p>{props.messageSent}</p>    
+      
+
   return (
-      <div className='weatherCard'>weatherCard</div>
+      <div className='weatherCard'>
+        <div className='cardHeader'>
+          <span id='count'>{props.country.country}</span> 
+          <p id='state'>{props.data.name}</p> 
+        </div>
+        <div className='temp-con'>
+          <h1 id='temp'>{props.main.temp ? temp() : ""}<sup>o</sup>C</h1>
+          <div className='values'>
+            <div className='temp'>
+            <p>{props.main.temp_min ? temp_min() : ""}<sup>o</sup>C</p>
+            <p>{props.main.temp_max ? temp_max() : ""}<sup>o</sup>C</p> 
+              </div>
+            <div className='physics'>
+            <p>{props.main.pressure}</p>
+            <p>{props.main.humidity}</p>
+            </div>
+          </div>
+          
+        
+        </div>
+        <div className='dataDescription'>
+            <div className='weather'>
+              <p>{props.weather.main}</p>  
+              <p>{props.weather.description}</p>
+
+            </div>
+            <div className='speed'>
+              <p>{props.wind.speed}</p>
+            </div>
+          </div>
+      </div>
+
   )
 }
 
-export default weatherCard
+export default WeatherCard
