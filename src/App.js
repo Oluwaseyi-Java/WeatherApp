3395e9ed2990e4b60ca5819f249a88c5544d3d61
Oluwaseyi-Java <adeseyitu@gmail.com>
2022-03-19 20:41:03 +0100

    resolving axios problem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './components/header';
 import Search from './components/search';
 import './App.css';
 import WeatherCard from './components/weatherCard';
 import LocationText from './components/location-text';
 
+
 function App() {
+
+  // let [responseData, setResponseData] = React.useState('')
+  // let [message, setMessage] = React.useState('')
+
+  
+
+  const onAdd = (value) => {
+
+
+    // let ID = "75d4d68ab258bd16c2defa27bb9c5a7c"
+    // let url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${ID}`
+    //   setMessage('Loading...')
+    // axios(url)
+    //   .then((response)=>{
+    //     setResponseData(response.data)
+    //     setMessage('')
+    //     console.log(response)
+    //   })
+    //   .catch((error) => {
+    //     setMessage('Error')
+    //     console.log(error)
+    //   })
+  }
+
   return (
     <div className="App">
       <Header/>
-      <Search/>
+      <Search onPressed={onAdd} />
       <LocationText/>
       <WeatherCard/>
     </div>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import searchIcon from "./image/search.png"
 
-const Search = () => {
+const Search = ({ onPressed }) => {
 
-  const [value,setValue]=React.useState([])
+  const [value,setValue]=React.useState([
 
-  const onSubmit=()=>{
-    
+  ])
+
+  const onSubmit=(e)=>{
+    e.preventDefault()
+
+    if(!value){
+      alert("Enter a city")
+      return
+    }
+    onPressed(value)
+
+    setValue("")
   }
   return (
     <div className='search'>
     <div className='search-wrapper'>
-        <input className='input' type="text" placeholder="Search"/>
+        <input className='input'  
+          type="text" 
+          placeholder="Enter your city"
+          value={value}
+          onChange={(e)=>
+            setValue(e.target.value)
+          }
+        />
         <div className='search-img' onClick={onSubmit}>
             <img alt='search' src={searchIcon}/>
         </div>
